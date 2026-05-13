0d6e6f74d575be6d8b6d2ac6299e923501360795
이광수 <qwe402700@naver.com>
2026-05-13 18:50:31 +0900

    feat: UI System Development
    - Added a bar at the top to display the participants' stamina, health, and portrait as UI.

---
+++ Assets/CWNU/Scripts/SimpleStatusUI.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleStatusUI : MonoBehaviour
+{
+    // 이미 배치된 UI이니 데이터 소스만 연결해주면 됩니다.
+    public TPSFixedMovement playerSource;
+    public EnemyAI enemySource;
+
+    public Image hpBar;
+    public Image staminaBar;
+
+    void Update()
+    {
+        // 데이터가 연결된 놈만 업데이트
+        if (playerSource != null)
+        {
+            hpBar.fillAmount = playerSource.currentHealth / playerSource.maxHealth;
+            staminaBar.fillAmount = playerSource.currentStamina / playerSource.maxStamina;
+        }
+        else if (enemySource != null)
+        {
+            hpBar.fillAmount = enemySource.currentHealth / enemySource.maxHealth;
+            staminaBar.fillAmount = enemySource.currentStamina / enemySource.maxStamina;
+        }
+    }
+}
